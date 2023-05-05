ab92cb45dff16bea137350f634ba1b213a210b92
Timur Kharisov <timur1776@gmail.com>
2023-05-05 13:05:58 +0300

    init bin

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+// .arguments('<filePath1> <filePath2>')
+// .option('-f, --format <type>', 'output format', 'stylish');
+// .action((filePath1, filePath2) => {
+//   console.log(genDiff(filePath1, filePath2, program.opts().format));
+// });
+program.parse();
